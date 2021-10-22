195511b1a56a0662cc38a94ef1baf47d8b95d6aa
Bunny <77222580+Bunny-03@users.noreply.github.com>
2021-10-22 18:14:26 +0530

    Update about.js

--- slashCommands/Information/about.js
+++ slashCommands/Information/about.js
@@ -2,7 +2,7 @@ const { MessageEmbed, CommandInteraction, Client, MessageButton, MessageActionRo
 
 module.exports = {
     name: "about",
-    description: "Show Lavamusic project information",
+    description: "Show project information",
 
     /**
      * 
@@ -20,25 +20,19 @@ module.exports = {
     .setLabel("Invite")
     .setStyle("LINK")
     .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot`),
-			new MessageButton()
-    .setLabel("GitHub")
-    .setStyle("LINK")
-    .setURL("https://github.com/brblacky/lavamusic"),
     new MessageButton()
     .setLabel("Support")
     .setStyle("LINK")
-    .setURL("https://discord.gg/gfcv94hDhv")
+    .setURL("https://discord.gg/98ePBCHaze")
 			);
 
       const mainPage = new MessageEmbed()
-            .setAuthor('LavaMusic', 'https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png')
-            .setThumbnail('https://media.discordapp.net/attachments/876035356460462090/888434725235097610/20210820_124325.png')
+            .setAuthor('Rhyme', 'https://media.discordapp.net/attachments/859359954598494218/887662609686482984/unknown.png?width=516&height=438')
+            .setThumbnail('https://media.discordapp.net/attachments/859359954598494218/887662609686482984/unknown.png?width=516&height=438')
             .setColor('#303236')
-            .addField('Creator', '[Blacky#6618](https://github.com/brblacky) And [Venom#9718](https://github.com/Venom9718/)', true)
-            .addField('Organization', '[Blacky](https://github.com/brblacky)', true)
-            .addField('Repository', '[Here](https://github.com/brblacky/lavamusic)', true)
+            .addField('Creator', 'BunnyPro#9146', true)
             .addField('\u200b',
-                `[LavaMusic](https://github.com/brblacky/lavamusic/) is [Blacky](https://github.com/brblacky) and [Venom](https://github.com/Venom9718)'s Was created by blacky and Venom. He really wants to make his first open source project ever. Because he wants more for coding experience. In this project, he was challenged to make project with less bugs. Hope you enjoy using LavaMusic!`
+                `Rhyme was created by Bunny. Hope you enjoy using Rhyme!`
             )
         await interaction.followUp({embeds: [mainPage], components: [row]});
     }
